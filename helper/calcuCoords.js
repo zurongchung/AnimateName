b31bd1e45624ee41d2aaf5524d946002a8e4cefd
zurongchung <zurongchung@gmail.com>
2016-09-18 13:05:45 -0700

    event listener to calculate

--- helper/calcuCoords.js
+++ helper/calcuCoords.js
@@ -6,22 +6,34 @@ or  (y2-y1) = (x2-x1)m
 By given an Known point(x1,y1) and either given a second x2 or y2
 we can calculate the unkonwn and get a new point that's on the slope
 */
+var w_ele = document.querySelector('input[name="width"]');
 var h_ele = document.querySelector('input[name="height"]');
+var division = document.querySelector('input[name="division"]');
+var gap = document.querySelector('input[name="gap"]').value;
+var quantity = document.querySelector('input[name="quantity"]').value;
 
 var GoldenRatio = 1.618;
 
-function Points(_w) {
-  this.w = _w;
-  this.h = this.w / GoldenRatio;
+// Attach event listener
+h_ele.addEventListener('change', newPoint, false);
+function newPoint() {
+  var p = new Points(h_ele.value, division.value);
+  w_ele.value = p.getWidth();
+}
+
+function Points(_h, _dis) {
+  this.h = _h;
+  this.dis = _dis;
+  this.w = this.letterWide();
   // Known coordinates
-  this.x = ;
-  this.y = ;
-  this.x1 = ;
-  this.y1 = ;
+  this.x = 0;
+  this.y = this.h;      // base lower-left corner
+  this.x1 = this.w / this.dis;
+  this.y1 = 0;
   // unkonwn coordinates
-  this.x2 = ;
-  this.y2 = member();
-  this.slope = slope();
+  this.x2 = 0;
+  this.y2 = this.member();
+  this.slope = this.slope();
 }
 Points.prototype.member = function() {
   // Given a x2 to calculate y2
@@ -34,6 +46,11 @@ Points.prototype.slope = function() {
 }
 
 // display height on the page
-Points.prototype.getHeight = function() {
-  return this.h;
+Points.prototype.getWidth = function() {
+  return this.w;
+}
+// how wide this letter is
+Points.prototype.letterWide = function() {
+  console.log(this.dis);
+  return Math.round((this.h * GoldenRatio) / this.dis);
 }
