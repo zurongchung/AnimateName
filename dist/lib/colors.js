1a264717f69ecdf0ad30956f15c41690ce258467
zurongchung <zurongchung@gmail.com>
2016-09-20 19:10:19 +0800

    add more color

--- dist/lib/colors.js
+++ dist/lib/colors.js
@@ -1,10 +1,12 @@
 // Define appearance of geometry shapes
 
 var Color = {
-  C1: [206, 53, 0],     // Aviation Orange
-  C2: [9, 86, 141],     // Blue
-  C3: [255, 187, 0],    // Lemon Yellow
-  C4: [69, 119, 37],    // Oxygen Tank Green
+  C1: [174, 218, 75],    // green
+  C2: [12, 144, 209],    // Preface Isolation
+  C3: [206, 53, 0],      // Aviation Orange
+  C4: [198, 12, 209],    // Oxygen Tank Green
+  C5: [106, 12, 209],    // ghostwriter, primary
+  C6: [24, 208, 175],    // mintasia
   getClr: function(_idx) {
     return Color['C' + _idx];
   },
